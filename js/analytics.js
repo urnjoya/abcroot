c488ab904dfb5e4401e9afc55d8bf50a8018aea9
Joya <urnjoya@gmail.com>
2026-03-05 15:12:55 +0530

    Update analytics.js

--- js/analytics.js
+++ js/analytics.js
@@ -13,3 +13,10 @@ function gtag() { dataLayer.push(arguments); }
 gtag('js', new Date());
 
 gtag('config', 'G-44K47CQEKZ');
+// 
+// <!-- Clarity tracking code for https://abcroot.xyz/ -->
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "vqy58i0lc9");
